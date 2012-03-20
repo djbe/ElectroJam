49d1f997b9205e283a79e45758de73b07679ce21
David Jennes <david.jennes@gmail.com>
2012-03-20 21:00:36 +0100

    Start on Bonjour code (just sample stuff for now)

--- src/com/davidjennes/ElectroJam/InstrumentService.java
+++ src/com/davidjennes/ElectroJam/InstrumentService.java
@@ -1,19 +1,34 @@
 package com.davidjennes.ElectroJam;
 
+import java.io.IOException;
 import java.util.Map;
+
+import javax.jmdns.JmDNS;
+import javax.jmdns.ServiceEvent;
+import javax.jmdns.ServiceListener;
 
 import android.app.Service;
 import android.content.Intent;
+import android.net.wifi.WifiManager;
+import android.net.wifi.WifiManager.MulticastLock;
 import android.os.IBinder;
 import android.os.RemoteException;
 import android.util.Log;
 
 public class InstrumentService extends Service {
 	private static final String TAG = "InstrumentService";
+	private static final String LOCK_NAME = "ElectroJamInstrument-BonjourLock";
+	private MulticastLock m_lock;
 	
 	@Override
     public void onCreate() {
         super.onCreate();
+        
+        // Acquire lock to be able to process multicast
+        WifiManager wifi = (WifiManager) getSystemService(android.content.Context.WIFI_SERVICE);
+        m_lock = wifi.createMulticastLock(LOCK_NAME);
+        m_lock.setReferenceCounted(true);
+        m_lock.acquire();
     }
 	
 	@Override
@@ -21,10 +36,43 @@ public class InstrumentService extends Service {
         return m_binder;
     }
 	
+	public void onDestroy() {
+        if (m_lock != null)
+        	m_lock.release();
+   }
+	
 	private final IInstrumentService.Stub m_binder = new IInstrumentService.Stub() {
 		@Override
 		public void loadSamples(Map samples) throws RemoteException {
-			Log.d(TAG, "loading samples");
+			try {
+				final JmDNS jmdns = JmDNS.create();
+				String type = "_workstation._tcp.local.";
+				
+				ServiceListener listener = new ServiceListener() {
+			        public void serviceResolved(ServiceEvent ev) {
+			            Log.d(TAG, "Service resolved: "
+			                     + ev.getInfo().getQualifiedName()
+			                     + " port:" + ev.getInfo().getPort());
+			        }
+			        public void serviceRemoved(ServiceEvent ev) {
+			        	Log.d(TAG, "Service removed: " + ev.getName());
+			        }
+			        public void serviceAdded(ServiceEvent event) {
+			            // Required to force serviceResolved to be called again
+			            // (after the first search)
+			            jmdns.requestServiceInfo(event.getType(), event.getName(), 1);
+			        }
+			    };
+			    jmdns.addServiceListener(type, listener);
+			    
+			    // ...
+			    
+			    jmdns.removeServiceListener(type, listener);
+			    jmdns.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 
 		@Override
