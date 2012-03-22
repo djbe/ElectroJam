c4ae3dd664dce63061fdeda29d63244c25df1561
David Jennes <david.jennes@gmail.com>
2012-03-22 14:23:28 +0100

    More work on client/server

--- src/com/davidjennes/ElectroJam/InstrumentService.java
+++ src/com/davidjennes/ElectroJam/InstrumentService.java
@@ -1,12 +1,18 @@
 package com.davidjennes.ElectroJam;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 import javax.jmdns.JmDNS;
 import javax.jmdns.ServiceEvent;
+import javax.jmdns.ServiceInfo;
 import javax.jmdns.ServiceListener;
 
 import android.app.Service;
@@ -16,79 +22,149 @@ import android.net.wifi.WifiManager.MulticastLock;
 import android.os.IBinder;
 import android.os.RemoteException;
 import android.util.Log;
+import android.widget.Toast;
 
 public class InstrumentService extends Service {
 	private static final String TAG = "InstrumentService";
 	private static final String LOCK_NAME = "ElectroJamInstrument-BonjourLock";
+	private static final String TYPE = "_workstation._tcp.local.";
+	private final static Random RANDOM = new Random();
+	
 	private MulticastLock m_lock;
+	private JmDNS m_jmdns;
+	private Map<Integer, ServiceInfo> m_services;
+	private Socket m_socket;
+	private PrintWriter m_writer;
 	
     public void onCreate() {
         super.onCreate();
+        m_socket = null;
+        m_writer = null;
         
         // Acquire lock to be able to process multicast
         WifiManager wifi = (WifiManager) getSystemService(android.content.Context.WIFI_SERVICE);
         m_lock = wifi.createMulticastLock(LOCK_NAME);
         m_lock.setReferenceCounted(true);
         m_lock.acquire();
+        
+        // init JmDNS
+        try {
+			m_jmdns = JmDNS.create();
+			m_jmdns.addServiceListener(TYPE, m_listener);
+		} catch (IOException e) {
+			e.printStackTrace();
+			m_jmdns = null;
+		}
     }
 	
     public IBinder onBind(Intent intent) {
-        return m_binder;
+    	return m_binder;
     }
 	
-	public void onDestroy() {
-        if (m_lock != null)
-        	m_lock.release();
-   }
+    public void onDestroy() {
+    	if (m_jmdns != null) {
+		    try {
+		    	m_jmdns.removeServiceListener(TYPE, m_listener);
+				m_jmdns.close();
+			} catch (IOException e) {}    		
+    	}
+    	
+    	if (m_lock != null)
+    		m_lock.release();
+	}
 	
+    /**
+     * Service implementation
+     */
 	private final IInstrumentService.Stub m_binder = new IInstrumentService.Stub() {
 		public List availableServers() throws RemoteException {
-			return null;
+			synchronized(m_services) {
+				List<Integer> result = new ArrayList<Integer>();
+				
+				for (Integer id : m_services.keySet())
+					result.add(id);
+				
+				return result;
+			}
 		}
 		
 		public Map serverInfo(int id) throws RemoteException {
-			return null;
+			synchronized(m_services) {
+				if (m_services.containsKey(id)) {
+					Map<String, String> result = new HashMap<String, String>();
+					result.put("name", m_services.get(id).getName());
+					result.put("description", m_services.get(id).getNiceTextString());
+					return result;
+				} else
+					return null;
+			}
 		}
 		
 		public void connect(int id) throws RemoteException {
+			ServiceInfo info = null;
+			
+			// get service info
+			synchronized(m_services) {
+				if (m_services.containsKey(id))
+					info = m_services.get(id);
+			}
+			
+			// does it still exist
+			if (info == null)
+				Toast.makeText(getApplicationContext(), R.string.client_error_connect, Toast.LENGTH_SHORT).show();
+			else {
+				try {
+					m_socket = new Socket(info.getHostAddresses()[0], info.getPort());
+					m_writer = new PrintWriter(m_socket.getOutputStream(), true);
+				} catch (UnknownHostException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
 		}
 		
 		public void loadSamples(Map samples) throws RemoteException {
-			try {
-				final JmDNS jmdns = JmDNS.create();
-				String type = "_workstation._tcp.local.";
-				
-				ServiceListener listener = new ServiceListener() {
-			        public void serviceResolved(ServiceEvent ev) {
-			            Log.d(TAG, "Service resolved: "
-			                     + ev.getInfo().getQualifiedName()
-			                     + " port:" + ev.getInfo().getPort());
-			        }
-			        public void serviceRemoved(ServiceEvent ev) {
-			        	Log.d(TAG, "Service removed: " + ev.getName());
-			        }
-			        public void serviceAdded(ServiceEvent event) {
-			            // Required to force serviceResolved to be called again
-			            // (after the first search)
-			            jmdns.requestServiceInfo(event.getType(), event.getName(), 1);
-			        }
-			    };
-			    jmdns.addServiceListener(type, listener);
-			    
-			    // wait
-			    try {
-			    	Thread.sleep(5000);
-			    } catch(InterruptedException e) {}
-			    
-			    jmdns.removeServiceListener(type, listener);
-			    jmdns.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+			Log.d(TAG, "Load samples.");
 		}
-
+		
 		public void sendEvent(int sample, int mode) throws RemoteException {
-			Log.d(TAG, "playing sample: " + sample + " mode: " + mode);
+			if (m_socket == null || m_writer == null)
+				return;
+			
+			Log.d(TAG, "Send event. sample: " + sample + " mode: " + mode);
+			m_writer.println("Sample: " + sample + " mode: " + mode);
 		}
 	};
+	
+	/**
+	 * Listener for ZeroConf events
+	 */
+	private final ServiceListener m_listener = new ServiceListener() {
+		public void serviceResolved(ServiceEvent ev) {
+			synchronized(m_services) {
+				m_services.put(RANDOM.nextInt(), ev.getInfo());
+			}
+        }
+		
+        public void serviceRemoved(ServiceEvent ev) {
+        	String name = ev.getInfo().getQualifiedName();
+        	int key = 0;
+        	
+        	synchronized(m_services) {
+        		for (Map.Entry<Integer, ServiceInfo> entry : m_services.entrySet())
+        			if (entry.getValue().getQualifiedName().equals(name)) {
+        				key = entry.getKey();
+        				break;
+        			}
+        		
+        		m_services.remove(key);
+			}
+        }
+        
+        public void serviceAdded(ServiceEvent event) {
+            // Required to force serviceResolved to be called again (after the first search)
+            m_jmdns.requestServiceInfo(event.getType(), event.getName(), 1);
+        }
+    };
 }
--- src/com/davidjennes/ElectroJam/ServerWorker.java
+++ src/com/davidjennes/ElectroJam/ServerWorker.java
@@ -1,12 +1,18 @@
 package com.davidjennes.ElectroJam;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
 
 import android.util.Log;
 
 public class ServerWorker extends Thread {
+	private static final String TAG = "ServerWorker";
+	
 	private Server m_server;
 	private Socket m_socket;
+	private BufferedReader m_reader;
 	private volatile boolean m_stop;
 	
 	/**
@@ -35,10 +41,14 @@ public class ServerWorker extends Thread {
 	 */
 	public void run() {
 		try {
-			Log.d("ServerWorker", "Client connected!");
-			while (!m_stop)
-				Thread.sleep(1000);
-		} catch (InterruptedException e) {
+			Log.d("ServerWorker", "Client connected! port: " + m_socket.getLocalPort());
+			m_reader = new BufferedReader(new InputStreamReader(m_socket.getInputStream()));
+			
+			while (!m_stop) {
+				String line = m_reader.readLine();
+				Log.d(TAG, "Received: " + line);
+			}
+		} catch (IOException e) {
 			e.printStackTrace();
 		} finally {
 			m_server.threadFinished(this);
