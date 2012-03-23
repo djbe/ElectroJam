5a6cf1607dd54a2f697f62e2b4295b8898d91f89
jptr <jnpatry@gmail.com>
2012-03-23 11:08:13 +0100

    add sounds (bass2, lead1-2, rithmic2, trigger1)

--- src/com/davidjennes/ElectroJam/TestActivity.java
+++ src/com/davidjennes/ElectroJam/TestActivity.java
@@ -27,21 +27,29 @@ public class TestActivity extends Activity {
         m_buttonSound.put(R.id.LooperSnare3, R.raw.snare3);
         m_buttonSound.put(R.id.LooperSnare4, R.raw.snare4);
         m_buttonSound.put(R.id.LooperBass1, R.raw.bass1);
-        m_buttonSound.put(R.id.LooperBass2, R.raw.bass3);
+        m_buttonSound.put(R.id.LooperBass2, R.raw.bass2);
         m_buttonSound.put(R.id.LooperBass3, R.raw.bass3);
         m_buttonSound.put(R.id.LooperBass4, R.raw.bass4);
         m_buttonSound.put(R.id.LooperRythmic1, R.raw.rythmic1);
-        m_buttonSound.put(R.id.LooperRythmic2, R.raw.bass1);
+        m_buttonSound.put(R.id.LooperRythmic2, R.raw.rythmic2);
         m_buttonSound.put(R.id.LooperRythmic3, R.raw.bass1);
         m_buttonSound.put(R.id.LooperRythmic4, R.raw.bass1);
-        m_buttonSound.put(R.id.LooperLead1, R.raw.bass1);
-        m_buttonSound.put(R.id.LooperLead2, R.raw.bass1);
+        m_buttonSound.put(R.id.LooperLead1, R.raw.lead1);
+        m_buttonSound.put(R.id.LooperLead2, R.raw.lead2);
         m_buttonSound.put(R.id.LooperLead3, R.raw.bass1);
         m_buttonSound.put(R.id.LooperLead4, R.raw.bass1);
         m_buttonSound.put(R.id.LooperFX1, R.raw.bass1);
         m_buttonSound.put(R.id.LooperFX2, R.raw.bass1);
         m_buttonSound.put(R.id.LooperFX3, R.raw.bass1);
         m_buttonSound.put(R.id.LooperFX4, R.raw.bass1);
+        
+        m_buttonSound.put(R.id.TriggerButton1, R.raw.trigger1);
+        m_buttonSound.put(R.id.TriggerButton2, R.raw.trigger1);
+        m_buttonSound.put(R.id.TriggerButton3, R.raw.trigger1);
+        m_buttonSound.put(R.id.TriggerButton4, R.raw.trigger1);
+        m_buttonSound.put(R.id.TriggerButton5, R.raw.trigger1);
+        m_buttonSound.put(R.id.TriggerButton6, R.raw.trigger1);
+
     }
     
     public void onCreate(Bundle savedInstanceState) {
