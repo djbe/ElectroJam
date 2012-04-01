5d0b05c739d8f96f9c3709308b32180f0aca08eb
David Jennes <david.jennes@gmail.com>
2012-04-01 04:33:48 +0200

    LooperInstrument is now separated from sound manager, interacts with it through interment service. TODO: finish remote sound manager <--> server

--- src/com/davidjennes/ElectroJam/LooperInstrument.java
+++ src/com/davidjennes/ElectroJam/LooperInstrument.java
@@ -20,14 +20,11 @@ import android.view.View;
 import android.widget.ToggleButton;
 
 import com.davidjennes.ElectroJam.Client.IInstrumentService;
-import com.davidjennes.ElectroJam.Sound.SoundManager;
-import com.davidjennes.ElectroJam.Sound.LocalSoundManager;
 
 public class LooperInstrument extends Activity {
 	private static final String TAG = "LooperInstrument";
 	
-	private SoundManager m_soundManager;
-	private Map<Integer, Integer> m_buttonSound, m_buttonProgress;
+	private Map<Integer, Integer> m_buttonSound, m_soundProgress;
 	private ProgressDialog m_progressDialog;
 	private IInstrumentService m_instrumentService;
     
@@ -37,6 +34,8 @@ public class LooperInstrument extends Activity {
         
         // show progress dialog during loading
         m_progressDialog = ProgressDialog.show(this, getString(R.string.working), getString(R.string.loading_sounds), true, false);
+        m_buttonSound = new HashMap<Integer, Integer>();
+        m_soundProgress = new HashMap<Integer, Integer>();
         
         // connect to bounded instrument service
         Intent intent = new Intent();
@@ -52,24 +51,24 @@ public class LooperInstrument extends Activity {
 		super.onConfigurationChanged(newConfig);
 		setContentView(R.layout.instrument_looper);
 		
+		// light up buttons for playing sounds
 		for (Map.Entry<Integer, Integer> entry : m_buttonSound.entrySet()) {
 			int sound = entry.getValue();
-			
-			// light up buttons for playing sounds
-			if (m_soundManager.isPlaying(sound))
-				((ToggleButton) findViewById(entry.getKey())).setChecked(true);
-			
-			// re-associate progress bars
-			if (m_buttonProgress.containsKey(entry.getKey()))
-				m_soundManager.setProgressBar(sound, findViewById(m_buttonProgress.get(entry.getKey())));
+//			if (m_soundManager.isPlaying(sound))
+//				((ToggleButton) findViewById(entry.getKey())).setChecked(true);
 		}
+		
+		// re-associate progress bars
+//		for (Map.Entry<Integer, Integer> entry : m_soundProgress.entrySet())
+//			m_soundManager.setProgressBar(entry.getKey(), findViewById(entry.getValue()));
     }
     
     public void onDestroy() {
         super.onDestroy();
         
-        for (Map.Entry<Integer, Integer> entry : m_buttonSound.entrySet())
-    		m_soundManager.unloadSound(entry.getValue());
+//        for (Map.Entry<Integer, Integer> entry : m_buttonSound.entrySet())
+//    		m_soundManager.unloadSound(entry.getValue());
+		unbindService(m_connection);
     }
     
     /**
@@ -90,13 +89,10 @@ public class LooperInstrument extends Activity {
     	
     	// either play or stop    	
 		try {
-			if (play) {
-				m_soundManager.playSound(m_buttonSound.get(view.getId()), looped);
+			if (play)
 				m_instrumentService.playSound(m_buttonSound.get(view.getId()), looped);
-			} else {
-		    	m_soundManager.stopSound(m_buttonSound.get(view.getId()));
+			else
 		    	m_instrumentService.stopSound(m_buttonSound.get(view.getId()));
-			}
 		} catch (RemoteException e) {
 			e.printStackTrace();
 		}
@@ -123,27 +119,32 @@ public class LooperInstrument extends Activity {
 	 */
     private class LoadSoundsTask extends AsyncTask<Void, Void, Void> {
 		protected Void doInBackground(Void... params) {
-			int button, progress, sound;
-			m_soundManager = new LocalSoundManager(getBaseContext());
-			m_buttonSound = new HashMap<Integer, Integer>();
-	        m_buttonProgress = new HashMap<Integer, Integer>();
-	        
-	        // Get associations data
+			// Get associations data
 	        TypedArray buttons = getResources().obtainTypedArray(R.array.buttons);
 	        TypedArray progressBars = getResources().obtainTypedArray(R.array.progress_bars);
-	        TypedArray sounds = getResources().obtainTypedArray(R.array.sounds);
+	        TypedArray soundsArray = getResources().obtainTypedArray(R.array.sounds);
 	        
-	        // load sounds and find progress bars
-	        for (int i = 0; i < buttons.length(); ++i) {
-	        	button = buttons.getResourceId(i, -1);
-	        	sound = m_soundManager.loadSound(sounds.getResourceId(i, -1));
-	        	
-	        	m_buttonSound.put(button, sound);
-	        	if (i < progressBars.length()) {
-	        		progress = progressBars.getResourceId(i, -1);
-	        		m_buttonProgress.put(button, progress);
-	        		m_soundManager.setProgressBar(sound, findViewById(progress));
-	        	}
+	        // get sound resource IDs
+	        int[] sounds = new int[soundsArray.length()];
+	        for (int i = 0; i < sounds.length; ++i)
+	        	sounds[i] = soundsArray.getResourceId(i, -1);
+	        
+	        // load sounds
+	        try {
+				sounds = m_instrumentService.loadSamples(sounds);
+			} catch (RemoteException e) {
+				e.printStackTrace();
+			}
+	        
+	        // associate buttons to sounds
+	        for (int i = 0; i < buttons.length(); ++i)
+	        	m_buttonSound.put(buttons.getResourceId(i, -1), sounds[i]);
+	        
+	        // associate sounds to progress bars
+	        for (int i = 0; i < progressBars.length(); ++i) {
+        		int progress = progressBars.getResourceId(i, -1);
+        		m_soundProgress.put(sounds[i], progress);
+//	        	m_soundManager.setProgressBar(sounds[i], findViewById(progress));
 	        }
 	        
 			return null;
